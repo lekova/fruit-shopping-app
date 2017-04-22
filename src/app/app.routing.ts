a71b91f18946fc2442126a6b07693a47181d4b76
lekova <lina.lekova@gmail.com>
2017-04-22 16:59:31 -0400

    add fruits route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,10 @@ import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
 import { FruitsListComponent } from './fruits-list/fruits-list.component';
 
 const appRoutes: Routes = [
-  { path: 'fruit/:id', component: FruitDetailComponent }
+  { path: '', redirectTo: '/fruits', pathMatch: 'full' },
+  { path: 'fruit/:id', component: FruitDetailComponent },
+  { path: 'fruits',     component: FruitsListComponent }
+
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
