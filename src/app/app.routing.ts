aaf1f64f68173c4b8cb7300524cad6610a6f2f46
Lina Lekova <lina.lekova@gmail.com>
2017-04-27 18:04:32 -0400

    Add routing to cart component from detail component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,13 @@
 import { Routes, RouterModule } from "@angular/router";
 import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
 import { FruitsListComponent } from './fruits-list/fruits-list.component';
+import { CartComponent } from './cart/cart.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/fruits', pathMatch: 'full' },
   { path: 'fruit/:id', component: FruitDetailComponent },
-  { path: 'fruits',     component: FruitsListComponent }
-
+  { path: 'fruits', component: FruitsListComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
